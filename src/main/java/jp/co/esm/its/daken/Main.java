64864d3b7bc5fd2843f1c9252c9d0dcdce5ddc00
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2017-09-29 11:09:03 +0900

    initaial commit
    Hello World Application with sparkJava and GAE

---
+++ src/main/java/jp/co/esm/its/daken/Main.java
@@ -0,0 +1,25 @@
+package jp.co.esm.its.daken;
+
+import static spark.Spark.*;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+import spark.servlet.SparkApplication;
+
+public class Main implements SparkApplication {
+	public static void main(String[] args) {
+		new Main().init();
+	}
+
+	@Override
+	public void init() {
+		get("/hello", (req, res) -> "Hello World");
+	}
+
+	// Use Servlet annotation to define the Spark filter without web.xml:
+	@WebFilter(filterName = "SparkInitFilter", urlPatterns = { "/*" }, initParams = {
+			@WebInitParam(name = "applicationClass", value = "jp.co.esm.its.daken.Main") })
+	public static class SparkInitFilter extends spark.servlet.SparkFilter {
+	}
+}
