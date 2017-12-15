d4a1cd46517c110d97443ebefbeef4de69d07a31
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2017-12-15 13:06:52 +0900

    fix indent

--- src/main/kotlin/jp/co/esm/its/daken/AuthResource.kt
+++ src/main/kotlin/jp/co/esm/its/daken/AuthResource.kt
@@ -13,40 +13,40 @@ import spark.Spark.*
 
 class AuthResource {
 
-    private val verifier: GoogleIdTokenVerifier
+  private val verifier: GoogleIdTokenVerifier
 
-    init {
-        verifier = createGoogleIdTokenVerifier();
-        setup()
+  init {
+    verifier = createGoogleIdTokenVerifier();
+    setup()
+  }
+
+  private fun createGoogleIdTokenVerifier(): GoogleIdTokenVerifier {
+    val transport = UrlFetchTransport.getDefaultInstance()
+    val jsonFactory = JacksonFactory.getDefaultInstance()
+    return GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+            .setAudience(Collections.singletonList(Main.clientId()))
+            // Or, if multiple clients access the backend:
+            //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+            .build()
+  }
+
+  private fun setup() {
+    post("/auth", createVerifyTokenHandler())
+  }
+
+  private fun createVerifyTokenHandler() = Route({ req, res ->
+    val idTokenString = req.queryParams("idToken")
+    val idToken: GoogleIdToken? = verifier.verify(idTokenString)
+    if (idToken == null) {
+      res.status(400)
+    } else {
+      val domain = idToken.payload.hostedDomain
+      if ("esm.co.jp" != (domain)) {
+        res.status(400)
+      } else {
+        res.status()
+      }
     }
-
-    private fun createGoogleIdTokenVerifier(): GoogleIdTokenVerifier {
-        val transport = UrlFetchTransport.getDefaultInstance()
-        val jsonFactory = JacksonFactory.getDefaultInstance()
-        return GoogleIdTokenVerifier.Builder(transport, jsonFactory)
-                .setAudience(Collections.singletonList(Main.clientId()))
-                // Or, if multiple clients access the backend:
-                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
-                .build()
-    }
-
-    private fun setup() {
-        post("/auth", createVerifyTokenHandler())
-    }
-
-    private fun createVerifyTokenHandler() = Route({req, res ->
-        val idTokenString = req.queryParams("idToken")
-        val idToken: GoogleIdToken? = verifier.verify(idTokenString)
-        if (idToken == null) {
-            res.status(400)
-        } else {
-            val domain = idToken.payload.hostedDomain
-            if ("esm.co.jp" != (domain)) {
-                res.status(400)
-            } else {
-                res.status()
-            }
-        }
-    })
+  })
 
 }
