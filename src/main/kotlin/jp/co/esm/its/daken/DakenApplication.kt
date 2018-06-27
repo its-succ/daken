abe049148f44cba075c6af301504436a469a7065
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2018-06-27 22:05:06 +0900

    いらないimportを消した

--- src/main/kotlin/jp/co/esm/its/daken/DakenApplication.kt
+++ src/main/kotlin/jp/co/esm/its/daken/DakenApplication.kt
@@ -1,12 +1,10 @@
 package jp.co.esm.its.daken
 
 import com.google.appengine.api.datastore.DatastoreService
+import com.google.appengine.api.datastore.DatastoreServiceFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.support.SpringBootServletInitializer
-import com.google.appengine.api.datastore.DatastoreServiceFactory
-import com.googlecode.objectify.ObjectifyFactory
-import com.googlecode.objectify.ObjectifyService
 import org.springframework.context.annotation.Bean
 
 
